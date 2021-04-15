6b410562c8c3ba1cd642ab6768cb79cc37f4da41
sunabcun <47014905+sunabcun@users.noreply.github.com>
2021-04-15 15:09:34 -0400

    Update Trim.sh

--- Trim.sh
+++ Trim.sh
@@ -7,4 +7,6 @@
 #SBATCH -t 48:48:15
 #SBATCH -J Trim
 module load anaconda3.7.3
+module load fastqc
+
 /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/Practice/WGBS/tools/TrimGalore-0.6.6/trim_galore --illumina --trim-n --paired --cores 4 --fastqc *_R1.fastq.gz *_R2.fastq.gz
