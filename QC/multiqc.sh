00e61d20fab24fb74bf90e8c8c5be3ea87f11aa5
sunabcun <47014905+sunabcun@users.noreply.github.com>
2021-06-16 15:48:11 -0400

    Create multiqc.sh

---
+++ QC/multiqc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name="trap"
+#SBATCH -N 1
+#SBATCH --mem=16G
+#SBATCH -p medicine_q
+#SBATCH --mail-type="ALL"
+#SBATCH -t 48:48:15
+#SBATCH -J fastqc
+module load fastqc
+for i in *.bam; do
+fastqc $i
+done
+mv *fastqc.html /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/HISAT2/PostQC
+mv *fastqc.zip /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/HISAT2/PostQC
+
+cd /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/HISAT2/PostQC
+python3 -m multiqc ./
