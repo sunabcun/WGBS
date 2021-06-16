0183f3f96ede7aa3429473787268af7d428800fd
sunabcun <47014905+sunabcun@users.noreply.github.com>
2021-06-16 15:49:19 -0400

    Update multiqc.sh

--- QC/multiqc.sh
+++ QC/multiqc.sh
@@ -4,14 +4,7 @@
 #SBATCH --mem=16G
 #SBATCH -p medicine_q
 #SBATCH --mail-type="ALL"
-#SBATCH -t 48:48:15
-#SBATCH -J fastqc
-module load fastqc
-for i in *.bam; do
-fastqc $i
-done
-mv *fastqc.html /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/HISAT2/PostQC
-mv *fastqc.zip /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/HISAT2/PostQC
+#SBATCH -t 148:48:15
+#SBATCH -J multiqc
 
-cd /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/HISAT2/PostQC
-python3 -m multiqc ./
+python3 -m multiqc *_fastqc.zip
