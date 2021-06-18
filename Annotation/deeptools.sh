f1bfeaae95bd3b47c36602b4f0c990cbd16df270
sunabcun <47014905+sunabcun@users.noreply.github.com>
2021-06-18 18:13:29 -0400

    Update deeptools.sh

--- Annotation/deeptools.sh
+++ Annotation/deeptools.sh
@@ -11,6 +11,12 @@ for i in *.gz; do
  bedGraphToBigWig ${i%.bedGraph.gz}.sorted.bedGraph $refP/GRCh38.primary_assembly.genome.fa.fai $outP/${i%.bedGraph.gz}.bw
 done
 
+for i in in *.bedGraph;do
+LC_COLLATE=C sort -k1,1 -k2,2n $i > ${i%.sorted.bedGraph}_sort2.bedGraph
+done
+
+
+
 computeMatrix reference-point -S ${outP}/${ID1}.input${ID1}_R1_001_bismark_bt2_pe.deduplicated.bw ${outP}/${ID2}.input${ID2}_R1_001_bismark_bt2_pe.deduplicated.bw ${outP}/${ID3}.input${ID3}_R1_001_bismark_bt2_pe.deduplicated.bw -R ../References/transcripts.noexpressed.0.bed --referencePoint TSS -a 2000 -b 2000 --outFileName ${outP}/all.matrix_Genes_noexpressed.0.tab.gz --outFileNameMatrix ${outP}/all.matrix_Genes_noexpressed.0.tab
 
 computeMatrix reference-point -S ${outP}/${ID1}.input${ID1}_R1_001_bismark_bt2_pe.deduplicated.bw ${outP}/${ID2}.input${ID2}_R1_001_bismark_bt2_pe.deduplicated.bw ${outP}/${ID3}.input${ID3}_R1_001_bismark_bt2_pe.deduplicated.bw -R ../References/transcripts.expressed.0.bed --referencePoint TSS -a 2000 -b 2000 --outFileName ${outP}/all.matrix_Genes_expressed.0.tab.gz --outFileNameMatrix ${outP}/all.matrix_Genes_expressed.0.tab
