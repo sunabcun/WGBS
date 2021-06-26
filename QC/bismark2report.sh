cd40bec3db749756c2dc4a7e3ac5eaf96d903302
sunabcun <47014905+sunabcun@users.noreply.github.com>
2021-06-26 14:27:31 -0400

    Update bismark2report.sh

--- QC/bismark2report.sh
+++ QC/bismark2report.sh
@@ -5,25 +5,14 @@
 #SBATCH -p medicine_q
 #SBATCH --mail-type="ALL"
 #SBATCH -t 128:48:15
-#SBATCH -J Mbias
-for i in *.deduplicated.bam; do
+#SBATCH -J BISMARK2REPORT
+
+for i in *_PE_report.txt; do
 /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/WGBS/Tools/Bismark-0.23.0/bismark2report \
 --dir ./output \
---output ${i
--p \
---merge_non_CpG \
---gzip \
---mbias_only \
---no_overlap \
---output /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/WGBS/BISMARK/Methylation/Mbias \
-$i \
---parallel 8 --buffer_size 72G \
---genome_folder /gpfs/research/medicine/sequencer/NovaSeq/Outputs_fastq/2020_Outputs/Akash_Gunjan_05-19-2020_Yuna-samples/WGBS/Reference
+--output ${i%_PE_report.txt}.bismark.report.html \
+--alignment_report $i \
+--dedup_report ${i%_PE_report.txt}_pe.deduplication_report.txt \
+--splitting_report ${i%_PE_report.txt}_pe.deduplicated_splitting_report.txt \
+--mbias_report ${i%_PE_report.txt}_pe.deduplicated.M-bias.txt
 done
-
-bismark2report --dir $outP --output ${name}.bismark.report.html --alignment_report $bamP/${name}.input${name}_R1_001_bismark_bt2_PE_report.txt --dedup_report $bamP/${name}.input${name}_R1_001_bismark_bt2_pe.deduplication_report.txt --splitting_report $outP/${name}.input${name}_R1_001_bismark_bt2_pe.deduplicated_splitting_report.txt --mbias_report $outP/${name}.input${name}_R1_001_bismark_bt2_pe.deduplicated.M-bias.txt
-
-P5-1.P5-1_R1_bismark_bt2_pe.deduplicated.M-bias.txt
--rw-rw-r-- 1 ys16b medicine   911 Jun  9 07:17 P5-1.P5-1_R1_bismark_bt2_pe.deduplicated_splitting_report.txt
--rw-rw-r-- 1 ys16b medicine   291 Apr 27 17:52 P5-1.P5-1_R1_bismark_bt2_pe.deduplication_report.txt
--rw-r--r-- 1 ys16b medicine  1986 Apr 26 14:15 P5-1.P5-1_R1_bismark_bt2_PE_report.txt
